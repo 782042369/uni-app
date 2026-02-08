560efa2eb4091e1dcb62f21d335364834b2c1faa
waibuzheng <782042369@qq.com>
2026-02-08 21:11:40 +0800

    chore(types): 清理未使用的组件类型定义
    - 移除 DemoBlock 组件类型
    - 移除 PrivacyPopup 组件类型

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -7,11 +7,9 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    DemoBlock: typeof import('./../components/DemoBlock.vue')['default']
     GlobalLoading: typeof import('./../components/GlobalLoading.vue')['default']
     GlobalMessage: typeof import('./../components/GlobalMessage.vue')['default']
     GlobalToast: typeof import('./../components/GlobalToast.vue')['default']
-    PrivacyPopup: typeof import('./../components/PrivacyPopup.vue')['default']
     WdActionSheet: typeof import('wot-design-uni/components/wd-action-sheet/wd-action-sheet.vue')['default']
     WdButton: typeof import('wot-design-uni/components/wd-button/wd-button.vue')['default']
     WdConfigProvider: typeof import('wot-design-uni/components/wd-config-provider/wd-config-provider.vue')['default']
